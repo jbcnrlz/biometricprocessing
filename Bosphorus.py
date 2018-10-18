f63e030d21b8c4126f01e14b2c33d45b150484b6
Joao Cardia <jbcnrlz@gmail.com>
2018-10-18 10:32:18 +0200

    Arquivo principal e arquivo do database

---
+++ Bosphorus.py
@@ -0,0 +1,196 @@
+from baseClasses.DatabaseProcessingUtility import *
+from BosphorusTemplate import *
+from helper.functions import outputObj, loadOBJ, minmax, getDirectoriesInPath
+from TemplateNode import *
+from RotateFace import *
+import os, glob, copy, numpy as np
+
+
+class Bosphorus(DatabaseProcessingUtility):
+
+    def saveNewDepthImages(self):
+        for t in self.templates:
+            t.saveNewDepth()
+
+    def saveTemplateImage(self):
+        for t in self.templates:
+            t.save(True)
+            t.outputMarks(True)
+
+    def loadTemplateImage(self):
+        for t in self.templates:
+            t.loadImage()
+
+    def loadSymmFilledImages(self):
+        for t in self.templates:
+            t.loadSymFilledImage()
+
+    def getImageType(self):
+        return self.__imageType
+
+    def setImageType(self, value):
+        if (value == 'Depth'):
+            self.__imageType = os.path.join('Depth', 'DepthBMP')
+        else:
+            self.__imageType = value
+
+    imageType = property(getImageType, setImageType)
+
+    def __init__(self, path, imageType, filePose=['N']):
+        super().__init__(path)
+        self.imageType = imageType
+        self.filePose = filePose
+
+    def getFilesFromDirectory(self,path):
+        returnFiles = []
+        for f in glob.glob(path + '/*.'+self.extensionFile[self.imageType]):
+            returnFiles.append(f)
+
+        return returnFiles
+
+    def getTemplateType(self, fileName):
+        explodedFile = fileName.split('.')
+        explodedFile = explodedFile[0].split('_')
+        return explodedFile.pop()
+
+    def feedTemplates(self):
+        for subject in getDirectoriesInPath(self.databasePath):
+            directories = self.getFilesFromDirectory(os.path.join(self.databasePath,subject))
+            for d in directories:
+                for ft in self.filePose:
+                    if ft in d:
+                        subjectNumber = int(subject[-3:])
+                        euTemp = BosphorusTemplate(os.path.join(self.databasePath,subject,d),subjectNumber)
+                        self.templates.append(euTemp)
+                        break
+
+    def getDatabaseRepresentation(self):
+        returnRepr = [[], []]
+        for template in self.templates:
+            returnRepr[0].append(template.features)
+            returnRepr[1].append(template.itemClass)
+        return returnRepr
+
+    def getTemplateFromSubject(self, subject):
+        returnTemplates = []
+        for template in self.templates:
+            if (template.itemClass == subject):
+                returnTemplates.append(template)
+        return returnTemplates
+
+    def loadNewDepthImage(self):
+        for t in self.templates:
+            t.loadNewDepthImage()
+
+    def loadRotatedFaces(self, angles):
+        addTemplates = []
+        for t in self.templates:
+            print("Gerando copia de " + t.rawRepr)
+            for a in angles:
+                print("Angulo = " + str(a))
+                nobj = copy.deepcopy(t)
+                with im.open(nobj.rawRepr[0:-4] + '_rotate_' + str(a) + '_newdepth.bmp') as currImg:
+                    nobj.image = np.asarray(currImg)
+                    nobj.rawRepr = nobj.rawRepr[0:-4] + '_rotate_' + str(a) + '_newdepth.bmp'
+                addTemplates.append(nobj)
+
+        self.templates = self.templates + addTemplates
+
+    def loadRotatedFacesPC(self, angles):
+        addTemplates = []
+        rface = RotateFace()
+        for t in self.templates:
+            print("Gerando copia de " + t.rawRepr)
+            for a in angles:
+                print("Angulo = " + str(a))
+                rty = rface.getRotationMatrix(a, 'y')
+                nobj = copy.deepcopy(t)
+                pathBroken = nobj.rawRepr.split(os.path.sep)
+                fileName = pathBroken[-1]
+                pathBroken = os.path.sep.join(pathBroken[:-2])
+                fileName = fileName.split('_')
+                if (fileName[-1] == 'symmetricfilled.obj'):
+                    fileName = '_'.join(fileName[0:-1])
+                else:
+                    fileName = '_'.join(fileName)[0:-4]
+                pathBroken = os.path.join(pathBroken, 'Depth', 'DepthBMP', fileName + '_rotate_' + str(a) + '.obj')
+
+                nobj.rawRepr = pathBroken
+                nobj.faceMarks = rface.multiplyMatrices(nobj.faceMarks, rty)
+                addTemplates.append(nobj)
+
+        self.templates = self.templates + addTemplates
+
+    def generateDatabaseFile(self, path, faces, otherBases=None, outputDesired='SVMTorchFormat', excludeFiles=[]):
+        currSearch = self.templates
+        if otherBases:
+            for o in otherBases:
+                currSearch = currSearch + o.templates
+
+        currFiles = []
+        for f in currSearch:
+            originalPathFile = f.rawRepr.split(os.path.sep)
+            originalPathFile = originalPathFile[-1].split('_')[-1]
+            print(originalPathFile)
+            print(excludeFiles)
+            if not (originalPathFile in excludeFiles):
+                print('Entrou')
+                if (f.typeTemplate in faces):
+                    for ss in range(len(faces[f.typeTemplate])):
+                        if f.folderTemplate == faces[f.typeTemplate][ss]:
+                            currFiles.append(f)
+
+        return getattr(self, outputDesired)(currFiles, path)
+
+    def SVMTorchFormat(self, data, path):
+        f = open(path, 'w')
+        f.write(str(len(data)) + ' ' + str(len(data[0].features) + 1) + '\n')
+        for t in data:
+            print("Imprimindo linha")
+            f.write(' '.join([str(e) for e in t.features]) + ' ' + str(t.itemClass - 1) + '\n')
+        f.close()
+        return []
+
+    def caffeFormat(self, data, path):
+        f = open(path, 'w')
+        for t in data:
+            f.write(' '.join([str(e) for e in t.features]) + '\n')
+        f.close()
+        return []
+
+    def h5Format(self, data, path):
+        import h5py
+        X = np.zeros((len(data), len(data[0].features)))
+        Y = np.zeros((len(data), 1))
+        for t in range(len(data)):
+            X[t] = data[t].features
+            Y[t] = data[t].itemClass
+
+        with h5py.File(path, 'w') as H:
+            H.create_dataset('X', data=X)
+            H.create_dataset('Y', data=Y)
+        with open(path[:-3] + '_list.txt', 'w') as L:
+            L.write(path)
+
+    def imageForCaffe(self, data, path):
+        dataNum = 0
+        for t in data:
+            dataNum = dataNum + 1
+            fixedFeature = np.array(t.features).reshape(int(len(t.features) / 14), 14)
+            f = open(os.path.join(path, str(t.itemClass) + '_' + t.typeTemplate + '_' + str(dataNum) + '.txt'), 'w')
+            for i in fixedFeature:
+                finalPrint = ''
+                for j in i:
+                    finalPrint = finalPrint + str(j) + " "
+                f.write(finalPrint + '\n')
+            f.close()
+
+        return []
+
+    def generateCharsClasses(self, data, path):
+        returnArray = [[], []]
+        for t in data:
+            returnArray[0].append(t.features)
+            returnArray[1].append(t.itemClass)
+
+        return returnArray
---
+++ main_bosphorus.py
@@ -0,0 +1,61 @@
+import argparse
+from Bosphorus import *
+from tdlbp import *
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Process and extract FRGC database')
+    parser.add_argument('-p','--pathdatabase',help='Path for the database',required=True)
+    parser.add_argument('-t', '--typeoffile',choices=['Depth', 'NewDepth', 'Range','Matlab'], help='Type of files (Depth, NewDepth, Range,Matlab)', required=True)
+    parser.add_argument('-op', '--operation',choices=['pp', 'fe', 'both'], default='both', help='Type of operation (pp - PreProcess, fe - Feature Extraction, both)', required=False)
+    parser.add_argument('-f', '--pathtrainingfile', default=None,help='Path for the training file', required=False)
+    parser.add_argument('-c', '--parcal', default=False,type=bool, help='Should execute in parallell mode?', required=False)
+    parser.add_argument('-ap', '--points',type=int,default=None,help='Quantity of points',required=False)
+    parser.add_argument('-r', '--radius',type=int,default=None, help='Size of radius', required=False)
+    parser.add_argument('-s', '--steps', default=None, help='Pre-Processing steps, class names separated with _ parameters starts wth : and separated with ,', required=False)
+    parser.add_argument('-v', '--faceVariation', default='N', help='Type of face, separated by _', required=False)
+    parser.add_argument('-gImg', '--pathImages', default='/home/joaocardia/PycharmProjects/biometricprocessing/generated_images_lbp_frgc', help='Path for image signature', required=False)
+    args = parser.parse_args()
+
+    print('Iniciando...')
+    print(args)
+
+    gallery = Bosphorus(args.pathdatabase,args.typeoffile,args.faceVariation)
+    gallery.feedTemplates()
+
+    tdlbp = ThreeDLBP(8,14,[gallery])
+    tdlbp.fullPathGallFile = args.pathImages
+    if not args.steps is None:
+        ppSteps = args.steps.split('_')
+        for p in ppSteps:
+            className = None
+            parameters = None
+            kwargsList = None
+            if ':' in p:
+                parameters = p.split(':')
+                className = parameters[0]
+                parameters = parameters[1].split(',')
+                kwargsList = {}
+                for pr in parameters:
+                    lParameters = pr.split('=')
+                    kwargsList[lParameters[0]] = eval(lParameters[1])
+
+            else:
+                className = p
+
+            module = __import__(className)
+            class_ = getattr(module,className)
+            if kwargsList is None:
+                tdlbp.preProcessingSteps = class_()
+            else:
+                tdlbp.preProcessingSteps = class_(**kwargsList)
+
+    if args.operation in ['both','pp']:
+        tdlbp.preProcessing(True,args.parcal)
+        gallery.saveNewDepthImages()
+
+    if args.operation in ['both', 'fe']:
+        tdlbp.featureExtraction(args.points,args.radius,args.parcal)
+
+    if not args.pathtrainingfile is None:
+        galeryData = gallery.generateDatabaseFile(args.pathtrainingfile)

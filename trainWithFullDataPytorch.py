69b98cef30680a9828c8eecaa660724aa08fd513
Joao Cardia <jbcnrlz@gmail.com>
2018-11-05 17:22:01 +0100

    Implementação do Pytorch

---
+++ trainWithFullDataPytorch.py
@@ -0,0 +1,54 @@
+from helper.functions import generateData, generateFoldsOfData, generateImageData, loadFoldFromFolders
+import networks.PyTorch.jojo as jojo, argparse, numpy as np, torch, torch.optim as optim, torch.nn.functional as F
+import torch.utils.data, shutil, os
+
+def save_checkpoint(state,is_best,filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train Deep Models')
+    parser.add_argument('-p', '--pathBase', default='generated_images_lbp_frgc', help='Path for faces', required=False)
+    parser.add_argument('-b', '--batch', type=int, default=500, help='Size of the batch', required=False)
+    parser.add_argument('-c', '--classNumber', type=int, default=466, help='Quantity of classes', required=False)
+    parser.add_argument('-t', '--runOnTest', type=bool, default=False, help='Run on test data', required=False)
+    parser.add_argument('-e', '--epochs', type=int, default=10, help='Epochs to be run', required=False)
+    args = parser.parse_args()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    imageData, classesData = generateData(args.pathBase)
+    imageData = np.array(generateImageData(imageData)) / 255.0
+    classesData = np.array(classesData)
+
+    if os.path.exists('training_full_pytorch'):
+        shutil.rmtree('training_full_pytorch')
+
+    os.makedirs('training_full_pytorch')
+
+    muda = jojo.GioGio(args.classNumber)
+    muda.to(device)
+
+    qntBatches = imageData.shape[0] / args.batch
+
+
+    if np.amin(classesData) == 1:
+        foldGalleryClasses = classesData - 1
+
+    imageData = torch.from_numpy(np.rollaxis(imageData, 3, 1)).float()
+    classesData = torch.from_numpy(classesData).to(device)
+    tdata = torch.utils.data.TensorDataset(imageData, classesData)
+    train_loader = torch.utils.data.DataLoader(tdata, batch_size=args.batch, shuffle=True)
+
+    optimizer = optim.SGD(muda.parameters(), lr=0.01, momentum=0.5)
+
+    for ep in range(args.epochs):
+        for bIdx, (currBatch, currTargetBatch) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = muda(currBatch.to(device))
+            loss = F.cross_entropy(output, currTargetBatch.to(device))
+            loss.backward()
+            optimizer.step()
+            print('\r[%d, %05d de %05d] loss: %.3f' % (ep + 1, bIdx + 1, qntBatches, loss.item()), end='\r')
+
+    print('Terminou')

cb7eb82b6460b943af27861ec34cc412cb309963
jbcnrlz <jbcnrlz@gmail.com>
2019-09-02 19:09:36 +0200

    Colocando Lock3D Dataset

--- networks/PyTorch/jojo.py
+++ networks/PyTorch/jojo.py
@@ -312,17 +312,17 @@ class GioGio(nn.Module):
 
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(384*5*5, 2048),
+            nn.Linear(384*5*5, 4096),
             nn.ReLU(inplace=True),
-            MaxoutDynamic(1024, 2048),
+            #MaxoutDynamic(int(4096 / 2), 4096),
             nn.Dropout(),
-            nn.Linear(2048, 2048),
+            nn.Linear(4096, 4096),
         )
 
         self.softmax = nn.Sequential(
             nn.ReLU(inplace=True),
-            MaxoutDynamic(1024, 2048),
-            nn.Linear(2048, classes)
+            #MaxoutDynamic(1024, 2048),
+            nn.Linear(4096, classes,bias=False)
         )
 
     def forward(self, x):
@@ -330,3 +330,44 @@ class GioGio(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return  self.softmax(x), x
+
+'''
+class GioGio(nn.Module):
+
+    def calculateSize(self,dim,layer,inputSize):
+        padding = layer.padding if (type(layer.padding) is not list) else layer.padding[dim]
+        dilation = layer.dilation if (type(layer.dilation) is not list) else layer.dilation[dim]
+        kernel_size = layer.kernel_size if (type(layer.kernel_size) is not list) else layer.kernel_size[dim]
+        stride = layer.stride if (type(layer.stride) is not list) else layer.stride[dim]
+        return int(((inputSize+(padding*2)-dilation*(kernel_size-1)-1) / stride) + 1)
+
+    def __init__(self,classes,imageInput=(100,100),in_channels=4):
+        self.imageInput = imageInput
+        super(GioGio,self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=8, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(384*5*5, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 384*5*5)
+        x = self.classifier(x)
+        return x
+'''
